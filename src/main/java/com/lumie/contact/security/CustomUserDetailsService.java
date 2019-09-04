bd16e774b7275ab75dc53a13936057fd75b7c6f7
lukmie <lmienicki@gmail.com>
2019-09-04 23:18:21 +0200

    Spring Security initialized.

---
+++ src/main/java/com/lumie/contact/security/CustomUserDetailsService.java
@@ -0,0 +1,14 @@
+package com.lumie.contact.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
